fd404298e54be36be6ecb8874c33f743b21d6b07
NainaniJatinZ <nainani.jatin.0@gmail.com>
2021-11-27 23:16:24 +0530

    Presentation update

--- data_handler.py
+++ data_handler.py
@@ -1,18 +1,22 @@
 import cv2
 import os
 import numpy as np
-img = cv2.imread("images\somaiya.jpeg", cv2.IMREAD_GRAYSCALE)
-# img_f = cv2.resize(img, (150,200))
+
+
+# TO CREATE .NPY FILES FROM IMAGES
+
+
+path = "images\somaiya.jpeg"  # Change this to the image of your choice
+img = cv2.imread(path, cv2.IMREAD_GRAYSCALE) 
 (thresh, im_bw) = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-print(np.unique(im_bw))
+print(np.unique(im_bw))  # This will give an binary image with 0s and 255s
 
-
-for i, v1 in enumerate(im_bw):
+for i, v1 in enumerate(im_bw):      # as we need a binary array with 0s and 1s we will use the following code
     for j, v2 in enumerate(v1):
         if v2 == 255:
             im_bw[i, j] = 1
 
-np.save('data\somaiya.npy', im_bw)
+np.save('data\somaiya.npy', im_bw) #saving in npy format, change the path here as well
 cv2.imshow("img", im_bw)
 cv2.waitKey(0) 
 cv2.destroyAllWindows() 
